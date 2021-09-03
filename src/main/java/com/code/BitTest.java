57ecb756eac953cf78c6c1be6826e7c1c1441170
xz <1187567532@qq.com>
2021-09-03 14:21:36 +0800

    位运算练习

--- src/main/java/com/code/BitTest.java
+++ src/main/java/com/code/BitTest.java
@@ -9,21 +9,42 @@ package com.code;
 public class BitTest {
 
     public static void main(String[] args) {
-        System.out.println(bitMulti(3, 98));
+        System.out.println(1 / 64);
         System.out.println(bitAdd(3, 98));
+        System.out.println(bitSub(3, 98));
+        System.out.println(bitMulti(3, 98));
         System.out.println(div(95, 3));
+        System.out.println(3 + (3 << 1) + (3 << 3));
+        bitMapTest();
     }
 
+    /**
+     * bit 加法的原理 则是
+     * 1. 得到两数的 无进位二进制 x = a ^ b
+     * 2. 得到两数的 进位二进制 y = a & b << 1
+     * 3. 如果进位 y == 0, 意味着本次加 无进位, 直接返回 x
+     * 4. 如果进位 y == 1, 重复 1 - 2步,  a,b 此时 就是 x,y
+     */
     public static int bitAdd(int a, int b) {
         int result = 0;
+        // 进位不等于 0
         while (b != 0) {
-            int noneCarry = a;
-            a = (b ^ noneCarry);
-            b = (b & noneCarry) << 1;
-            result = a;
+            // 先保存结果集是本次 无进位结果
+            result = (b ^ a);
+            // 计算进位信息
+            b = (b & a) << 1;
+            // 覆盖 a, 当有进位信息时 后续循环则还需要计算。
+            a = result;
         }
 
         return result;
+    }
+
+    /**
+     * 那么减法只需要将  被减数 + (减少数取反 = ~b + 1);
+     */
+    public static int bitSub(int a, int b) {
+        return bitAdd(a, bitAdd(~b, 1));
     }
 
     public static int bitAdd1(int a, int b) {
@@ -46,10 +67,12 @@ public class BitTest {
      *  任选一个二进制开始, 依次遍历二进制位, 0 不变, 1 则加上 另一个二进制 << 当前进制位
      *  例如 从 4 开始
      *  1. 第0个进制位 0 所以 0000 0000
-     *  2. 第1个进制位 1 所以 0000 0000 左移一位 0000 0110 可以理解为这里拆分为 2 * 3
+     *  2. 第1个进制位 1 所以 0000 0011 左移一位 0000 0110 可以理解为这里拆分为 2 * 3
      *  3. 第2个进制位 0 所以 0000 0000
-     *  4. 第3个进制位 1 所以 0000 0000 左移三位 0011 0000 这里拆分为了 8 * 3
-     *  5. 遍历完毕, 三个进制 相加的到 0001 1110 = 2 * 3 + 8 * 3 = 30;
+     *  4. 第3个进制位 1 所以 0000 0011 左移三位 0001 1000 这里拆分为了 8 * 3
+     *  5. 遍历完毕, 上诉步骤结果集相加的到
+     *  0001 1110 = 2 * 3 + 8 * 3 = 30;
+     *  0001 1110 = (3 << 1) + (3 << 3)
      */
     public static int bitMulti(int one, int two) {
         int result = 0;
@@ -76,7 +99,7 @@ public class BitTest {
      * a 去除 b << 1后 0000 0101
      * << 0 <= a, 那么 c 此时 0000 0011;
      * a 去除 b << 0后 0000 0000
-     * b >= a, 此时运算完毕,
+     * 此时 b > a, 运算完毕,
      * c = 0000 0011 = 3;
      * @param a
      * @param b
@@ -97,6 +120,47 @@ public class BitTest {
         return result;
     }
 
+    /**
+     * 给定一个数 n. 计算出该范围内 每个值对应二进制位1的个数
+     * @param n
+     * @return
+     */
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        if (n == 0) {
+            return result;
+        }
+        result[1] = 1;
+        for (int i = 2; i <= n; i ++) {
+            if (i % 2 == 0) {
+                // 为什么偶数可以直接得到 i / 2 后的个数？
+                /**
+                 * 在二进制中, 偶数最后一位必定为0, 那么 i >> 1 位的1进制位一定是和
+                 * 结果对应的进制位相同,
+                 * 例如
+                 * 12 = 0000 1100
+                 * 6 = 0000 0110;
+                 * 12 / 2 = 6; 12 >> 1
+                 */
+                result[i] = result[i >> 1];
+            } else {
+                /**
+                 * 而奇数最后位必定为 1, 那么只需要 >> 1位后 补上被删除的 1 即可
+                 */
+                result[i] = result[i >> 1] + 1;
+            }
+
+        }
+        return result;
+    }
+
+    static int[] countBits1(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i ++) {
+            result[i] = result[i >> 1] + (i & 1);
+        }
+        return result;
+    }
 
     /**
      * 构建一个BitMap 判断某个数是否存在
@@ -105,27 +169,49 @@ public class BitTest {
         private long[] bitMap;
 
         /**
+         * 根据可存储最大值定义位图大小。
+         * @param maxVal
+         */
+        public BigMap(int maxVal) {
+            bitMap = new long[(maxVal >> 6) + 1];
+        }
+
+        /**
          * 新增时的思路:
          * 1. 一个long 占 64位, 那么可以以一 bit 标识一个数字, 一个long 则可以表示 0 - 63
          * 2. 由此, 我们可以通过 >> 6 (64) 计算出某个数所在long位置，通过取余 得到该数所在二进制位
          */
         public void add(int num) {
-
+            bitMap[num >> 6] |= (1L << (num & 63));
         }
 
         /**
-         * 而删除, 无非就是将num所在数组下的long 对应的二进制位置为 0
+         * 而删除, 无非就是将num所在数组下的long 对应的二进制位 置为 0
          */
         public void delete(int num) {
-
+            bitMap[num >> 6] &= (~(1L << (num & 63)));
         }
 
         /**
          * 判断是否存在则 获取num 所在数组节点下 long的二进制位 是否为1
          */
         public boolean contains(int num) {
-
-            return false;
+            return ((bitMap[num >> 6] >> (num & 63)) & 1) == 1L;
         }
+    }
+
+    public static void bitMapTest() {
+        BigMap map = new BigMap(1024);
+        map.add(1);
+        map.add(2);
+        map.add(3);
+        map.add(4);
+        map.delete(2);
+        System.out.println(map.contains(1));
+        System.out.println(map.contains(2));
+        System.out.println(map.contains(4));
+        System.out.println(map.contains(5));
+        System.out.println(map.contains(3));
+
     }
 }
